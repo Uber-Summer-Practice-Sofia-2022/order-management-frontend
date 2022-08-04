16e433c283a9359e3db08e83da4da52066997d9f
Daniel <dannysht27@gmail.com>
2022-08-04 10:39:00 +0300

    added useCallback for status page

--- src/components/page/StatusPage.jsx
+++ src/components/page/StatusPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import LoadingContainer from '../common/LoadingContainer';
 import { useParams } from 'react-router-dom';
@@ -13,7 +13,7 @@ export default function StatusPage() {
 
   const { orderId } = useParams();
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     setDataLoading(true);
     const orderDataReceived = await fetch(`${ORDERAPI_URL}/order/${orderId}`);
     const orderDataStatus = orderDataReceived.status;
@@ -21,11 +21,11 @@ export default function StatusPage() {
     setOrderData(orderDataJSON.Order);
     setDataRequestStatus(orderDataStatus);
     setDataLoading(false);
-  };
+  }, [orderId]);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   return (
     <Container style={{
